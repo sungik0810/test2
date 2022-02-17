2c502abdff4ba188cf6f7f868b7a8cbf058b6bfe
sungik0810 <89075013+sungik0810@users.noreply.github.com>
2022-02-17 15:58:50 +0900

    Add files via upload

--- test.js
+++ test.js
@@ -1,12 +1,15 @@
 const getBackground = document.querySelector(".class");
 const getCircle = document.querySelector(".circle");
 const getrandomBackground = document.querySelector(".randomBackground");
+
+let backRandomColorNum = 3;
+
 //마우스 위치 찾기 -> 박스 마우스 위치로 이동시키기
 function mousePosition(event){
     let x = 0;
     let y = 0;
-    x = event.pageX-250;
-    y = event.pageY-250;
+    x = event.pageX-100;
+    y = event.pageY-100;
     
     getCircle.style.left = `${x}px`;
     getCircle.style.top = `${y}px`;
@@ -20,16 +23,16 @@ document.addEventListener("mousemove",mousePosition);
 
 
 //화면 색 바꾸기
-function changeColor(event){
-    let r = Math.round(Math.random()*255);
-    let g = Math.round(Math.random()*255);
-    let b = Math.round(Math.random()*255);
-    getBackground.style.backgroundColor = `rgb(${r},${g},${b})`;
-}
-document.addEventListener("click",changeColor);
+// function changeColor(event){
+//     let r = Math.round(Math.random()*255);
+//     let g = Math.round(Math.random()*255);
+//     let b = Math.round(Math.random()*255);
+//     getBackground.style.backgroundColor = `rgb(${r},${g},${b})`;
+// }
+// document.addEventListener("click",changeColor);
 
 
-for(let i = 0; i < 50; i++){
+for(let i = 0; i < backRandomColorNum; i++){
 const makeRandomBackground = document.createElement("div");
 getrandomBackground.appendChild(makeRandomBackground);
 makeRandomBackground.classList.add("randomColor");
@@ -42,23 +45,28 @@ let innerWidth = window.innerWidth;
 let innerHeight = window.innerHeight;
 let randomLeft = Math.round(Math.random()*innerWidth);
 let randomTop = Math.round(Math.random()*innerHeight);
-getrandomBackgroundNum.style.boxShadow = `1000px 1000px 200px 200px rgba(255,${window["rBg"+i]},${window["rBb"+i]},0.5)`;
-getrandomBackgroundNum.style.left = `${randomLeft-1500}px`;
-getrandomBackgroundNum.style.top = `${randomTop-1500}px`;
+getrandomBackgroundNum.style.background = `radial-gradient(circle at 50% 50%, rgba(255, ${window["rBg"+i]},${window["rBb"+i]}, 1) 0%, rgba(0, 0, 0, 0)60%, rgba(0, 0, 0, 0) 100%)`;
+getrandomBackgroundNum.style.left = `${randomLeft-400}px`;
+getrandomBackgroundNum.style.top = `${randomTop-400}px`;
+getrandomBackgroundNum.style.zIndex = backRandomColorNum-i;
 
 
 }
 
-let getcircleColor = document.querySelector(".circle");
+
+function showCoords(event) {
 
 
-function functionName(){
-    for(let i=0;i<50;i++){
-    getcircleColor.style.boxShadow =`200px 200px 10px rgba(255,${window["rBg"+i]},${window["rBb"+i]},0.5)`;
-    }
+
+        for(let i = 0; i<backRandomColorNum; i++){
+            function functionName(){
+                getCircle.style.background = `radial-gradient(circle at 50% 50%, rgba(255, ${window["rBg"+i]},${window["rBb"+i]}, 1) 0%, rgba(0, 0, 0, 0)20%, rgba(0, 0, 0, 0) 100%)`;
+                
+            }//function
+            document.querySelector(`.randomColor${i}`).addEventListener("mousemove",functionName);
+        }
+        
+
+
 }
-document.addEventListener("mousemove",functionName);
-
-        //if( 마우스가 getrandomBackgroundNum 위에 있는지 확인 == true){
-        //getCircle.style.boxShadow = `200px 200px 100px rgba(${rBr2},${rBg2},${rBb2},0.5)`;
-        //}
+document.addEventListener("mouseover",showCoords);
